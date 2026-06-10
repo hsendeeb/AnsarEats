c0228e53b83fb83f7767898fec3b77bd6a579ad7
hsen <hsendeeb2@gmail.com>
2026-06-10 13:23:09 +0300

    remove tickets reference

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
 
 Route::get('/login',[AuthController::class,'login']);
-Route::get('/tickets',function () {
-    return Ticket::all();
-});
